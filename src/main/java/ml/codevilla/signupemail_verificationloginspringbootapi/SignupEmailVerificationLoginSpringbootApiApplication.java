73112479647755e50da866b4ad52ecd8327bd131
JxstWieslaw <wieslawsamushonga01@gmail.com>
2022-02-02 02:55:33 +0530

    Initial Commit
    Starter project for API

---
+++ src/main/java/ml/codevilla/signupemail_verificationloginspringbootapi/SignupEmailVerificationLoginSpringbootApiApplication.java
@@ -0,0 +1,13 @@
+package ml.codevilla.signupemail_verificationloginspringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupEmailVerificationLoginSpringbootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupEmailVerificationLoginSpringbootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ml/codevilla/signupemail_verificationloginspringbootapi/SignupEmailVerificationLoginSpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.codevilla.signupemail_verificationloginspringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupEmailVerificationLoginSpringbootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
